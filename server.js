5ae9924dbca67668f0e5e00637b43d3d3ddf434e
Chris Petrevski <c_petrevski@hotmail.com>
2022-01-06 17:55:37 +1100

    updated code

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork', {
-  useFindAndModify: false,
+mongoose.connect('mongodb://localhost:27017/socialnetwork', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
+
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`Listening on Port:${PORT}`));
